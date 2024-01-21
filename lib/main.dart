7db6709580f526faada475177fd4ecb89204535e
Nurbolot Muratbekuulu <savagenurkg@gmail.com>
2024-01-21 16:46:22 +0600

    app theme modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:messenger_ui/features/chat/data/datasources/mocks/chats.dart';
+import 'package:messenger_ui/features/chat/data/datasources/mocks/messages.dart';
+import 'package:messenger_ui/features/chat/design_dimens.dart';
+import 'package:messenger_ui/features/chat/domain/entities/chat_entity.dart';
+import 'package:messenger_ui/features/chat/presentation/bloc/cubit/chat_cubit.dart';
+import 'package:messenger_ui/features/chat/presentation/pages/contact_list_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MessengerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MessengerApp extends StatelessWidget {
+  const MessengerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Messenger UI",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => ChatCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Messenger UI",
+        theme: _buildAppTheme(),
+        home: const ContactListPage(),
       ),
-      home: const HomePage(),
     );
   }
-}
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Home',
-        ),
-      ),
-    );
+  ThemeData _buildAppTheme() {
+    return ThemeData(
+        primarySwatch: Colors.blue,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(DesignDimens.largeRadius),
+            gapPadding: DesignDimens.smallPadding,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(DesignDimens.largeRadius),
+            borderSide: BorderSide(width: .5, color: Colors.grey),
+            gapPadding: DesignDimens.smallPadding,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(DesignDimens.largeRadius),
+            gapPadding: DesignDimens.smallPadding,
+          ),
+        ));
   }
 }
